c68b0e1f2eec406fe11b83b8c4465ea26b3155f5
adarsh-sng <adarshishu79@gmail.com>
2025-08-30 03:00:26 +0530

    init:project bootstrap with react and hono

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+// console.log(app)
+const correctWord =["hello","adieu","tiger"]
+app.post('/validate-word', async (c) => {
+  const word = await c.req.json()
+  console.log(word);
+  // console.log(c.json(correctWord.includes(word) ? "you pass" : "you fail"))
+   console.log(correctWord.includes(word) ? "you pass" : "you fail")
+  return c.json(correctWord.includes(word) ? "you pass" : "you fail");
+})
+
+export default app
